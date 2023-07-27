94b5315ab471b2c4e5ee569a79ff4085555be6aa
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2023-07-27 11:23:28 +0530

    [Apps.] Regenerated demo for device_cdc_host_msd_pic32mk_gp_db
    MH3-82523

--- apps/device_cdc_host_msd/firmware/src/config/pic32mk_gp_db/definitions.h
+++ apps/device_cdc_host_msd/firmware/src/config/pic32mk_gp_db/definitions.h
@@ -60,11 +60,11 @@
 #include "driver/usb/usbfs/drv_usbfs.h"
 #include "driver/usb/usbfs/drv_usbfs.h"
 #include "system/time/sys_time.h"
-#include "usb/usb_chapter_9.h"
-#include "usb/usb_host.h"
 #include "usb/usb_device_cdc.h"
 #include "usb/usb_cdc.h"
 #include "peripheral/coretimer/plib_coretimer.h"
+#include "usb/usb_chapter_9.h"
+#include "usb/usb_host.h"
 #include "system/fs/sys_fs.h"
 #include "system/fs/sys_fs_media_manager.h"
 #include "system/fs/sys_fs_fat_interface.h"
@@ -85,6 +85,12 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* Device Information */
+#define DEVICE_NAME			 "PIC32MK1024GPE100"
+#define DEVICE_ARCH			 "MIPS"
+#define DEVICE_FAMILY		 "PIC32MK1402"
+#define DEVICE_SERIES		 "PIC32MK"
 
 /* CPU clock frequency */
 #define CPU_CLOCK_FREQUENCY 96000000
