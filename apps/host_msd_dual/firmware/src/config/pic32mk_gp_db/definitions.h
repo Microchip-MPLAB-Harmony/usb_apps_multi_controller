b783e881965c60e4949b4027afe3cd9ea526ffd7
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2023-07-27 11:23:30 +0530

    [Apps.] Regenerated demo for host_msd_dual_pic32mk_gp_db
    MH3-82529

--- apps/host_msd_dual/firmware/src/config/pic32mk_gp_db/definitions.h
+++ apps/host_msd_dual/firmware/src/config/pic32mk_gp_db/definitions.h
@@ -58,9 +58,9 @@
 #include "driver/usb/usbfs/drv_usbfs.h"
 #include "driver/usb/usbfs/drv_usbfs.h"
 #include "system/time/sys_time.h"
-#include "peripheral/coretimer/plib_coretimer.h"
 #include "usb/usb_chapter_9.h"
 #include "usb/usb_host.h"
+#include "peripheral/coretimer/plib_coretimer.h"
 #include "system/fs/sys_fs.h"
 #include "system/fs/sys_fs_media_manager.h"
 #include "system/fs/sys_fs_fat_interface.h"
@@ -81,6 +81,12 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* Device Information */
+#define DEVICE_NAME			 "PIC32MK1024GPE100"
+#define DEVICE_ARCH			 "MIPS"
+#define DEVICE_FAMILY		 "PIC32MK1402"
+#define DEVICE_SERIES		 "PIC32MK"
 
 /* CPU clock frequency */
 #define CPU_CLOCK_FREQUENCY 120000000
