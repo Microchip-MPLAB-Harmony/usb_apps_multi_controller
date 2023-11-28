4f29c808877dff26786e856538a43e6857727064
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2023-11-28 17:42:54 +0530

    [Apps.] Regenerated demo for device_cdc_host_msd_pic32cz_ca80_curiosity_ultra
    MH3-87427

--- apps/device_cdc_host_msd/firmware/src/config/pic32cz_ca80_curiosity_ultra/configuration.h
+++ apps/device_cdc_host_msd/firmware/src/config/pic32cz_ca80_curiosity_ultra/configuration.h
@@ -141,10 +141,10 @@ extern "C" {
 #define USB_HOST_MSD_LUN_NUMBERS              1
 
 
-#define USB_DEVICE_INSTANCES_NUMBER                         1
+#define USB_DEVICE_INSTANCES_NUMBER                         1U
 
 /* EP0 size in bytes */
-#define USB_DEVICE_EP0_BUFFER_SIZE                          64
+#define USB_DEVICE_EP0_BUFFER_SIZE                          64U
 
 /* The USB Device Layer will not initialize the USB Driver */
 #define USB_DEVICE_DRIVER_INITIALIZE_EXPLICIT
@@ -154,7 +154,7 @@ extern "C" {
 
 
 /* Number High Speed USB Driver instances */ 
-#define DRV_USBHS_INSTANCES_NUMBER                       2
+#define DRV_USBHS_INSTANCES_NUMBER                       2U
 
 /* Interrupt mode enabled */
 #define DRV_USBHS_INTERRUPT_MODE                          true
@@ -162,7 +162,7 @@ extern "C" {
 /* Enables Device Support */
 #define DRV_USBHS_DEVICE_SUPPORT                          true
 
-#define DRV_USBHS_ENDPOINTS_NUMBER                        3
+#define DRV_USBHS_ENDPOINTS_NUMBER                        3U
 
 /* Enable Host Support */
 #define DRV_USBHS_HOST_SUPPORT                            true
@@ -181,6 +181,15 @@ extern "C" {
 
 /* Alignment for buffers that are submitted to USB Driver*/ 
 #define USB_ALIGN  CACHE_ALIGN
+
+/* Maximum instances of CDC function driver */
+#define USB_DEVICE_CDC_INSTANCES_NUMBER                     1U
+
+
+/* CDC Transfer Queue Size for both read and
+   write. Applicable to all instances of the
+   function driver */
+#define USB_DEVICE_CDC_QUEUE_DEPTH_COMBINED                 3U
 
 // *****************************************************************************
 // *****************************************************************************
@@ -205,15 +214,6 @@ extern "C" {
 
 /* The maximum number of pipes that the USB Host layer can utilize. */
 #define USB_HOST_PIPES_NUMBER                               10
-
-/* Maximum instances of CDC function driver */
-#define USB_DEVICE_CDC_INSTANCES_NUMBER                     1
-
-
-/* CDC Transfer Queue Size for both read and
-   write. Applicable to all instances of the
-   function driver */
-#define USB_DEVICE_CDC_QUEUE_DEPTH_COMBINED                 3
 
 
 
