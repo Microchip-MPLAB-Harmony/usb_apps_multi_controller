7a42c3d3e3789dfc7e2bb098908931d9ebeb3823
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-05-29 15:07:05 +0530

    [Apps.] Regenerated demo for device_cdc_host_msd_pic32ck_gc01_curiosity_ultra
    MH3-93125

--- apps/device_cdc_host_msd/firmware/src/config/pic32ck_gc01_curiosity_ultra/configuration.h
+++ apps/device_cdc_host_msd/firmware/src/config/pic32ck_gc01_curiosity_ultra/configuration.h
@@ -203,6 +203,15 @@ extern "C" {
 #define USB_DEVICE_SOF_EVENT_ENABLE
 
 
+/* Maximum instances of CDC function driver */
+#define USB_DEVICE_CDC_INSTANCES_NUMBER                     1U
+
+
+/* CDC Transfer Queue Size for both read and
+   write. Applicable to all instances of the
+   function driver */
+#define USB_DEVICE_CDC_QUEUE_DEPTH_COMBINED                 3U
+
 // *****************************************************************************
 // *****************************************************************************
 // Section: USB Host Layer Configuration
@@ -226,15 +235,6 @@ extern "C" {
 
 /* The maximum number of pipes that the USB Host layer can utilize. */
 #define USB_HOST_PIPES_NUMBER                               10
-
-/* Maximum instances of CDC function driver */
-#define USB_DEVICE_CDC_INSTANCES_NUMBER                     1U
-
-
-/* CDC Transfer Queue Size for both read and
-   write. Applicable to all instances of the
-   function driver */
-#define USB_DEVICE_CDC_QUEUE_DEPTH_COMBINED                 3U
 
 
 
