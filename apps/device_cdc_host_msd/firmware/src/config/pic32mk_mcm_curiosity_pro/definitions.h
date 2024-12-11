d2020b356aef1a076e3f83c0a44c4b9f45dd2450
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-12-11 16:13:27 +0530

    Regenerated demo for device_cdc_host_msd_pic32mk_mcm_curiosity_pro
    MH3-106769

--- apps/device_cdc_host_msd/firmware/src/config/pic32mk_mcm_curiosity_pro/definitions.h
+++ apps/device_cdc_host_msd/firmware/src/config/pic32mk_mcm_curiosity_pro/definitions.h
@@ -59,10 +59,10 @@
 #include "driver/usb/usbfs/drv_usbfs.h"
 #include "driver/usb/usbfs/drv_usbfs.h"
 #include "system/time/sys_time.h"
-#include "usb/usb_chapter_9.h"
-#include "usb/usb_host.h"
 #include "usb/usb_device_cdc.h"
 #include "usb/usb_cdc.h"
+#include "usb/usb_chapter_9.h"
+#include "usb/usb_host.h"
 #include "peripheral/coretimer/plib_coretimer.h"
 #include "bsp/bsp.h"
 #include "system/fs/sys_fs.h"
@@ -87,13 +87,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "PIC32MK1024MCM100"
-#define DEVICE_ARCH			 "MIPS"
-#define DEVICE_FAMILY		 "PIC32MK1519"
-#define DEVICE_SERIES		 "PIC32MK"
+#define DEVICE_NAME          "PIC32MK1024MCM100"
+#define DEVICE_ARCH          "MIPS"
+#define DEVICE_FAMILY        "PIC32MK1519"
+#define DEVICE_SERIES        "PIC32MK"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 96000000
+#define CPU_CLOCK_FREQUENCY 96000000U
 
 // *****************************************************************************
 // *****************************************************************************
